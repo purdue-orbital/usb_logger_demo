1920b5b4b5edb8b78a2b89d7ee3cbad122c2cbe0
Ruhaan Bhargav <ruhaanbhargav6@gmail.com>
2024-11-18 20:50:42 -0500

    Made so that ADXL314 sets up in stream mode

--- src/adxl314.rs
+++ src/adxl314.rs
@@ -1,11 +1,12 @@
 use embedded_hal::i2c::I2c;
 
 const ADDR: u8 = 0x53;
+const FIFO_ADDR: u8 = 0x38;
 const REG_POWER_CTL: u8 = 0x2D;
 const REG_DATAX0: u8 = 0x32;
 
 pub fn get_ids(bus: &mut impl I2c) -> Result<[u8; 2], ()> {
-    let mut buf = [0_u8; 1];
+    let mut buf = [0; 1];
 
     let res = bus.write_read(ADDR, &[0x00], &mut buf);
 
@@ -14,6 +15,15 @@ pub fn get_ids(bus: &mut impl I2c) -> Result<[u8; 2], ()> {
     }
     
     Ok([0,0])
+}
+
+pub fn setup (bus: &mut impl I2c) {
+    let mut buf = [0; 1];
+    let _ = bus.write_read(ADDR, &[0x38], &mut buf);
+    // reset target bits to 0
+    buf[0] &= !0b1100_0000;
+    buf[0] |= 0b10 << 6;
+    let _ =bus.write(ADDR, &[0x38, buf[0]]);
 }
 
 pub fn read_acceleration(bus: &mut impl I2c) -> Result<(f32, f32, f32), ()> {
