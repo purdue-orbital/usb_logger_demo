26e3966f2e8c8a36461881fbeb53d98ed1d18a57
Cryptonoid2030 <137273973+Cryptonoid2030@users.noreply.github.com>
2025-01-26 13:23:31 -0500

    updated code according to library for testing

--- src/adxl314.rs
+++ src/adxl314.rs
@@ -5,33 +5,26 @@ const FIFO_ADDR: u8 = 0x38;
 const REG_POWER_CTL: u8 = 0x2D;
 const REG_DATAX0: u8 = 0x32;
 
-pub fn get_ids(bus: &mut impl I2c) -> Result<[u8; 2], ()> {
+pub fn get_ids(bus: &mut impl I2c) -> Result<[u8; 2]> {
     let mut buf = [0; 1];
 
-    let res = bus.write_read(ADDR, &[0x00], &mut buf);
-
-    if res.is_err() {
-        log::error!("{:?}", res);
-    }
+    let res = bus.write_read(ADDR, &[0x00], &mut buf).unwrap();
     
     Ok([0,0])
 }
 
 pub fn setup (bus: &mut impl I2c) {
     let mut buf = [0; 1];
-    let _ = bus.write_read(ADDR, &[0x38], &mut buf);
+    let _ = bus.write_read(ADDR, &[0x38], &mut buf).unwrap();
     // reset target bits to 0
     buf[0] &= !0b1100_0000;
     buf[0] |= 0b10 << 6;
     let _ = bus.write(ADDR, &[0x38, buf[0]]);
 }
 
-pub fn read_acceleration(bus: &mut impl I2c) -> Result<(f32, f32, f32), ()> {
+pub fn read_acceleration(bus: &mut impl I2c) -> Result<(f32, f32, f32)> {
     let mut buf = [0_u8; 6];
-    let res = bus.write_read(ADDR, &[REG_DATAX0], &mut buf);
-    if res.is_err(){
-        log::error!("{:?}", res);
-    }
+    let res = bus.write_read(ADDR, &[REG_DATAX0], &mut buf).unwrap();
     let x:f32 = i16::from_le_bytes([0, 1]) as f32 * 48.83;
     let y:f32 = i16::from_le_bytes([2, 3]) as f32 * 48.83;
     let z:f32 = i16::from_le_bytes([4, 5]) as f32 * 48.83;
