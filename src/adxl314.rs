cd627e770738be630a07b21174ca9974786369f8
Ruhaan Bhargav <ruhaanbhargav6@gmail.com>
2025-01-29 20:25:13 -0500

    Changed ADXL code to work with errors + Potential fix for constant error values

--- src/adxl314.rs
+++ src/adxl314.rs
@@ -5,13 +5,17 @@ const FIFO_ADDR: u8 = 0x38;
 const REG_POWER_CTL: u8 = 0x2D;
 const REG_DATAX0: u8 = 0x32;
 
-pub fn get_ids(bus: &mut impl I2c) {// -> Result<[u8; 2]> {
-    let mut buf = [0; 1];
+pub fn get_ids(bus: &mut impl I2c) -> Result<[u8; 2], i8> {
+    let mut buf = [0; 2]; // Expecting 2 bytes from the device
 
-    let res = bus.write_read(ADDR, &[0x00], &mut buf).unwrap();
-    
-    //Ok([0,0])
+    // Try to read from register 0x00
+    if let Err(e) = bus.write_read(ADDR, &[0x00], &mut buf) {
+        return Err(-1); // Return the error if communication fails
+    }
+
+    Ok(buf) // Successfully read 2 bytes, return them
 }
+
 
 pub fn setup (bus: &mut impl I2c) {
     let mut buf = [0; 1];
@@ -22,11 +26,16 @@ pub fn setup (bus: &mut impl I2c) {
     let _ = bus.write(ADDR, &[0x38, buf[0]]);
 }
 
-pub fn read_acceleration(bus: &mut impl I2c) {//-> Result<(f32, f32, f32)> {
+pub fn read_acceleration(bus: &mut impl I2c) -> Result<(f32, f32, f32), i8> {
     let mut buf = [0_u8; 6];
-    let res = bus.write_read(ADDR, &[REG_DATAX0], &mut buf).unwrap();
-    let x:f32 = i16::from_le_bytes([0, 1]) as f32 * 48.83;
-    let y:f32 = i16::from_le_bytes([2, 3]) as f32 * 48.83;
-    let z:f32 = i16::from_le_bytes([4, 5]) as f32 * 48.83;
-    //Ok((x, y, z))
+    
+    if let Err(e) = bus.write_read(ADDR, &[REG_DATAX0], &mut buf) {
+        return Err(-1);
+    }
+
+    let x: f32 = i16::from_le_bytes([buf[0], buf[1]]) as f32 * 48.83;
+    let y: f32 = i16::from_le_bytes([buf[2], buf[3]]) as f32 * 48.83;
+    let z: f32 = i16::from_le_bytes([buf[4], buf[5]]) as f32 * 48.83;
+
+    Ok((x, y, z))
 }
